fad1e0f1c607a57113f316e425b86adcbd4438b4
Rejoan2014 <rejoanru14@gmail.com>
2025-03-24 22:12:01 +0600

    added gsap animation

---
+++ assets/js/gsap.js
@@ -0,0 +1,141 @@
+gsap.utils.toArray('.fade-up').forEach((element) => {
+    gsap.from(element, {
+        y: -30,
+        opacity: 0, 
+        duration: 0.5, 
+        stagger: 0.3, 
+        scrollTrigger: {
+            trigger: element,
+            start: "top 80%", 
+            toggleActions: "play none none reverse",
+            markers: true, 
+        }
+    });
+});
+
+// Fade up effect on scroll
+gsap.utils.toArray('.fade-btn').forEach((element) => {
+    gsap.from(element, {
+        x:-200, // Slide up effect
+        opacity: 0,
+        duration: 2,
+        ease: "power2.out",
+        scrollTrigger: {
+            trigger: element,
+            start: "top 90%",
+            toggleActions: "play none none reverse"
+        }
+    });
+
+    // Hover effect
+    element.addEventListener('mouseenter', () => {
+        gsap.to(element, {
+            scale: 1.05,
+            opacity: 0.9,
+            duration: 0.2,
+            ease: "power2.out"
+        });
+    });
+
+    element.addEventListener('mouseleave', () => {
+        gsap.to(element, {
+            scale: 1,
+            opacity: 1,
+            duration: 0.2,
+            ease: "power2.out"
+        });
+    });
+});
+
+gsap.utils.toArray('.fadeInUpZoom').forEach((element) => {
+    gsap.from(element, {
+        y: 20,
+        opacity: 0,
+        duration: 1.5,
+        scale: 0.2,
+        scrollTrigger: {
+            trigger: element,
+            start: "top 85%", 
+            toggleActions: "play none none reverse",
+            // markers: true, 
+        }
+    });
+});
+
+gsap.utils.toArray('.fadeInScale').forEach((element) => {
+    gsap.from(element, {
+        opacity: 0,
+        scale: 0.8,
+        duration: 1,
+        ease: "power2.out",
+        scrollTrigger: {
+            trigger: element,
+            start: "top 85%",
+            toggleActions: "play none none reverse"
+        }
+    });
+});
+
+
+// Slide Left Animation
+gsap.utils.toArray('.slide-left').forEach((element) => {
+    gsap.from(element, {
+        x: -300, // Slide from left
+        opacity: 0,
+        duration: 1,
+        ease: "power2.out",
+        scrollTrigger: {
+            trigger: element,
+            start: "top 85%",
+            toggleActions: "play none none reverse",
+        }
+    });
+});
+
+// Slide Right Animation
+gsap.utils.toArray('.slide-right').forEach((element) => {
+    gsap.from(element, {
+        x: 300, // Slide from right
+        opacity: 0,
+        duration: 1,
+        ease: "power2.out",
+        scrollTrigger: {
+            trigger: element,
+            start: "top 85%",
+            toggleActions: "play none none reverse",
+        }
+    });
+});
+
+
+gsap.utils.toArray('.slide-leftToRight').forEach((element) => {
+    gsap.from(element, {
+        x: -70, 
+        opacity: 1,
+        duration: 1.5,
+        ease: "power2.out",
+        scrollTrigger: {
+            trigger: element,
+            start: "top 85%",
+            toggleActions: "play none none reverse",
+        }
+    });
+});
+
+
+
+
+
+gsap.utils.toArray('.slideUpFadeIn').forEach((element) => {
+    gsap.from(element, {
+        y: -50,
+        opacity: 0,
+        duration: 1,
+        ease: "power2.out",
+        scrollTrigger: {
+            trigger: element,
+            start: "top 90%",
+            toggleActions: "play none none reverse"
+        }
+    });
+});
